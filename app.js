487bd391df0ef19327777c39d38b00ec3e6bf042
yousrik20 <https://github.com/yousrik20>
2025-11-23 10:53:14 +0100

    lesson6

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 const Mydata = require("./models/mydataSchema");
 app.set("view engine", "ejs");
+app.use(express.static('public'))
+
+// auto refresh to apply static file changes CSS, JS, IMG...
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+ 
+ 
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+ 
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+
 
 app.get("/", (req, res) => {
   Mydata.find()
