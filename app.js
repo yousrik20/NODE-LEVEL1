2f2b22ff8fde96a862bb0ddb9df3bd7b29f10725
yousrik20 <https://github.com/yousrik20>
2025-11-23 10:22:45 +0100

    Apply .gitignore rules

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const Mydata = require("./models/mydataSchema")
+const Mydata = require("./models/mydataSchema");
+app.set("view engine", "ejs");
 
-  app.get("/", (req, res) => {
-    res.sendFile("./views/home.html", { root: __dirname });
-  });
+app.get("/", (req, res) => {
+  Mydata.find()
+    .then((result) => {
+      res.render("home", { mytitle: "Home Page" ,arr: result  });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get("/index.html", (req, res) => {
-    res.send("<h1>Data sended Successfully!</h1>" );
-  });
+  res.send("<h1>Data sended Successfully!</h1>");
+});
 
 mongoose
   .connect(
@@ -28,10 +35,13 @@ mongoose
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  const mydata= new Mydata(req.body);
-  mydata.save().then(() => {
-    res.redirect("/index.html");
-  } ).catch((err) => {
-    console.log(err);
-  });
+  const mydata = new Mydata(req.body);
+  mydata
+    .save()
+    .then(() => {
+      res.redirect("/index.html");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
