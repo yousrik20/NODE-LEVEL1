40a2853731f2c6daf554c39248e8bf429b38eecc
yousrik20 <https://github.com/yousrik20>
2025-11-23 09:35:35 +0100

    lesson4

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mydataSchema")
 
--
-app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", { root: __dirname });
-});
+  app.get("/", (req, res) => {
+    res.sendFile("./views/home.html", { root: __dirname });
+  });
 
-
+app.get("/index.html", (req, res) => {
+    res.send("<h1>Data sended Successfully!</h1>" );
+  });
 
 mongoose
   .connect(
@@ -22,3 +25,13 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const mydata= new Mydata(req.body);
+  mydata.save().then(() => {
+    res.redirect("/index.html");
+  } ).catch((err) => {
+    console.log(err);
+  });
+});
---
+++ models/mydataSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// define the Schema (the structure of the article)
+const articleSchema = new Schema({
+  userNameee: String,
+});
+
+// Create a model based on that schema
+const Mydata = mongoose.model("Mydata", articleSchema);
+ 
+ // export the model
+module.exports = Mydata;    
