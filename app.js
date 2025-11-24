e6917fc25c0a8d1ed8a5249703644e3f7c10c7e5
yousrik20 <https://github.com/yousrik20>
2025-11-24 11:14:47 +0100

    lesson12

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 const User = require("./models/customerSchema.js");
 app.set("view engine", "ejs");
 app.use(express.static('public'))
+var moment = require('moment'); // require
 
 // auto refresh to apply static file changes CSS, JS, IMG...
 
@@ -28,7 +29,7 @@ app.get("/", (req, res) => {
   console.log("--------------------");
   User.find()
     .then((result) => {
-      res.render("index",{arr:result});
+      res.render("index",{arr:result,moment:moment});
     })
     .catch((err) => {
       res.status(500).send("Error retrieving users: " + err);
@@ -37,17 +38,7 @@ app.get("/", (req, res) => {
   });
 
 
-app.get("/user/:id", (req, res) => {
-  // Result is Object
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/view",{obj:result});
-    })
-    .catch((err) => {
-      res.status(500).send("Error retrieving users: " + err);
-    });   
-    
-  });
+
 
 
 // Get Requests
@@ -64,6 +55,17 @@ app.get("/user/edit.html", (req, res) => {
   res.render("./user/edit");
 });
 
+app.get("/user/:id", (req, res) => {
+  // Result is Object
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/view",{obj:result,moment:moment});
+    })
+    .catch((err) => {
+      res.status(500).send("Error retrieving users: " + err);
+    });   
+    
+  });
 // Post Requests
 app.post("/user/add.html", (req, res) => {
   const user = new User(req.body);
--- models/customerSchema.js
+++ models/customerSchema.js
@@ -3,18 +3,17 @@ const Schema = mongoose.Schema;
 
 // define the Schema (the structure of the article)
 const userSchema = new Schema({
-  firstName:String,
-  lastName:String,
-  gender:String,
-  email:String,
-  phoneNumber:String,
-  age:Number,
-  country:String,
-  
-});
+  firstName: String,
+  lastName: String,
+  gender: String,
+  email: String,
+  phoneNumber: String,
+  age: Number,
+  country: String,
+},{timestamps: true});
 
 // Create a model based on that schema
 const User = mongoose.model("customer", userSchema);
- 
- // export the model
-module.exports = User;    
+
+// export the model
+module.exports = User;
