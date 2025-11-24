d3f5450d7641c299415ecef05ea42b9b5d0d67e5
yousrik20 <https://github.com/yousrik20>
2025-11-24 18:51:03 +0100

    lesson14

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 app.use(express.static('public'))
 var moment = require('moment'); // require
 
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 // auto refresh to apply static file changes CSS, JS, IMG...
 
 //const path = require("path");
@@ -51,11 +53,18 @@ app.get("/user/add.html", (req, res) => {
 });
 
 
-app.get("/user/edit.html", (req, res) => {
-  res.render("./user/edit");
+app.get("/edit/:id", (req, res) => {
+
+User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/edit",{obj:result,moment:moment});
+    })
+    .catch((err) => {
+      res.status(500).send("Error retrieving users: " + err);
+    });   
 });
 
-app.get("/user/:id", (req, res) => {
+app.get("/view/:id", (req, res) => {
   // Result is Object
   User.findById(req.params.id)
     .then((result) => {
@@ -81,6 +90,16 @@ app.post("/user/add.html", (req, res) => {
 })
 
 
+// Delete Request
+
+app.delete("/edit/:id", (req, res) => {
+  User.findByIdAndDelete(req.params.id).then(() => {
+      res.redirect("/");
+    }).catch((err) => {
+      console.log("Error Deleting the item: " + err);
+    });
+
+})
 
 // Database part
 mongoose
