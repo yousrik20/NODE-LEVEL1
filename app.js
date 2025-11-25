db431e19ee61d7109248df7d8893ae2da9ce0cd2
yousrik20 <https://github.com/yousrik20>
2025-11-25 10:23:50 +0100

    lesson15

--- app.js
+++ app.js
@@ -5,43 +5,37 @@ const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
 const User = require("./models/customerSchema.js");
 app.set("view engine", "ejs");
-app.use(express.static('public'))
-var moment = require('moment'); // require
+app.use(express.static("public"));
+var moment = require("moment"); // require
 
-var methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 // auto refresh to apply static file changes CSS, JS, IMG...
 
 //const path = require("path");
 //const livereload = require("livereload");
 //const liveReloadServer = livereload.createServer();
 //liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+
 //const connectLivereload = require("connect-livereload");
 //app.use(connectLivereload());
- 
+
 //liveReloadServer.server.once("connection", () => {
-  //setTimeout(() => {
-    //liveReloadServer.refresh("/");
- // }, 100);
+//setTimeout(() => {
+//liveReloadServer.refresh("/");
+// }, 100);
 //});
 
 app.get("/", (req, res) => {
   console.log("--------------------");
   User.find()
     .then((result) => {
-      res.render("index",{arr:result,moment:moment});
+      res.render("index", { arr: result, moment: moment });
     })
     .catch((err) => {
       res.status(500).send("Error retrieving users: " + err);
-    });   
-    
-  });
-
-
-
-
+    });
+});
 
 // Get Requests
 app.get("/", (req, res) => {
@@ -52,55 +46,58 @@ app.get("/user/add.html", (req, res) => {
   res.render("./user/add");
 });
 
-
 app.get("/edit/:id", (req, res) => {
-
-User.findById(req.params.id)
+  User.findById(req.params.id)
     .then((result) => {
-      res.render("user/edit",{obj:result,moment:moment});
+      res.render("user/edit", { obj: result, moment: moment });
     })
     .catch((err) => {
       res.status(500).send("Error retrieving users: " + err);
-    });   
+    });
 });
 
 app.get("/view/:id", (req, res) => {
   // Result is Object
   User.findById(req.params.id)
     .then((result) => {
-      res.render("user/view",{obj:result,moment:moment});
+      res.render("user/view", { obj: result, moment: moment });
     })
     .catch((err) => {
       res.status(500).send("Error retrieving users: " + err);
-    });   
-    
-  });
+    });
+});
 // Post Requests
 app.post("/user/add.html", (req, res) => {
-  const user = new User(req.body);
-
-  user
-    .save()
+  User.create(req.body)
     .then(() => {
-      res.redirect("/") ;
+      res.redirect("/");
     })
     .catch((err) => {
       res.status(500).send("Error adding user: " + err);
     });
-})
-
+});
 
 // Delete Request
 
 app.delete("/edit/:id", (req, res) => {
-  User.findByIdAndDelete(req.params.id).then(() => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => {
       res.redirect("/");
-    }).catch((err) => {
+    })
+    .catch((err) => {
       console.log("Error Deleting the item: " + err);
     });
+});
 
-})
-
+app.put("/edit/:id", (req, res) => {
+  User.updateOne({ _id: req.params.id }, req.body)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log("Error Updating the item: " + err);
+    });
+});
 // Database part
 mongoose
   .connect(
@@ -114,4 +111,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
